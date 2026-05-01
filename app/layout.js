c09b640d15a70571c38a7b3b021196e419e2849a
daniel21202 <danisa2129daniel@gmail.com>
2026-04-30 21:11:01 -0300

    dois

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
